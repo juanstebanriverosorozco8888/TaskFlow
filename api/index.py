d3946514cd59084d2f0e06e4519df314f8ff9db4
juan <juanstebanriverosorozco@gmail.com>
2026-06-24 17:57:43 -0500

    vercel deploy

--- api/index.py
+++ api/index.py
@@ -1,6 +1,18 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "config.settings"
+)
+
+from django.core.management import execute_from_command_line
+
+try:
+    execute_from_command_line(
+        ["manage.py", "migrate", "--noinput"]
+    )
+except Exception as e:
+    print("ERROR MIGRATE:", e)
 
 from django.core.wsgi import get_wsgi_application
 
